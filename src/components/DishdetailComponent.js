aa00a3e62b058c4cf983e036ee484d6a8edc97d4
Riya Prahlad <riya.prahlad@gmail.com>
2020-11-13 22:04:22 -0800

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-   CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({dish}) {
@@ -20,9 +21,9 @@ function RenderDish({dish}) {
         );
 }
 
-function RenderComments({dish}) {
-    if (dish != null) {
-        return dish.comments.map((comment) => (
+function RenderComments({comments}) {
+    if (comments != null) {
+        return comments.map((comment) => (
 
           <li key={comment.id}>
               <p>{comment.comment}</p>
@@ -37,17 +38,27 @@ function RenderComments({dish}) {
 
 const DishDetail = (props) => {
     return (
-      <div className='container'>
+      <div className="container">
         <div className="row">
-          <div  className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
-          </div>
-          <div  className="col-12 col-md-5 m-1">
-            <h4> Comments </h4>
-            <ul className='list-unstyled'>
-              <RenderComments dish={props.dish} />
-            </ul>
-          </div>
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <h4> Comments </h4>
+                <ul className="list-unstyled">
+                  <RenderComments comments={props.comments} />
+                </ul>
+            </div>
         </div>
       </div>
     );
