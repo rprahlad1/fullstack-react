e6ba271bbe985ec473637e8c9ffdb80f83aba9ab
Riya Prahlad <riya.prahlad@gmail.com>
2020-11-16 17:48:34 -0800

    Fetch Handling Errors

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -60,7 +60,7 @@ class Main extends Component {
               <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                 isLoading={this.props.dishes.isLoading}
                 errMess={this.props.dishes.errMess}
-                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
                 commentsErrMess={this.props.comments.errMess}
                 addComment={this.props.addComment} />
           );
